4c559c4ff8526c55ba3a21f1f6966893f80dca5a
Gonzalo García Corrales <115544633+gongarco@users.noreply.github.com>
2022-11-23 18:33:57 +0100

    google maps cards started

--- public/js/place-movies.js
+++ public/js/place-movies.js
@@ -28,6 +28,22 @@ function setMarkers(movies) {
             title: elm.name
         })
     })
+    // //Infowindow (quitar comentario cuando funcione todo)
+
+    // const infoWindowOptions = {
+    //     content: 'prueba de infowindow',
+    //     position: { lat: 40.751791436975466, lng: -73.9754376213823 }
+    // }
+
+    // const infoWindow = new google.maps.infoWindow(infoWindowOptions)
+
+    // const infoWindowOpenOptions = {
+    //     map: myMap
+    // }
+
+    // infoWindow.open(infoWindowOpenOptions)
+
+    // //Infowindow (quitar comentario cuando funcione todo)
 }
 
 
